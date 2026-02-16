2acdd2136975a5edceed88e18f4429c576038663
Vikash-Kumar001 <vikashkumarsudhi8527@gmail.com>
2026-02-16 21:00:46 +0530

    fix: fixed cors error

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,7 @@ const allowedOrigins = process.env.CLIENT_URL
       "http://localhost:5173",
       "http://localhost:3000",
       "https://wisestudent.org",
+      "https://www.wisestudent.org",
       "https://api.wisestudent.org",
       "https://16.112.67.176"
     ];
